3394ac887583999f9049f173ec94ce7e6afe96f0
StreakInTheSky <streakinthesky@gmail.com>
2017-05-25 00:25:25 -0700

    Add routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
-import App from './components/app';
+import Login from './components/login'
+import UserRegistration from './components/user_registration'
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={Login} />
+      <Route path="/registration" component={UserRegistration} />
+    </Router>
   </Provider>
   , document.querySelector('.container'));
