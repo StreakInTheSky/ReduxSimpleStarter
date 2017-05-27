adb5965da292dce4bc7b7b3afb2f6ccbb151c8d6
StreakInTheSky <streakinthesky@gmail.com>
2017-05-27 00:22:53 -0700

    Make show app

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+import Header from './header';
+
+export default function App(props) {
+  return (
+    <div className="container">
+      <Header />
+      {props.children}
+    </div>
+  );
 }
