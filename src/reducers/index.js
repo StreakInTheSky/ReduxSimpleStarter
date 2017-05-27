021fe04d198f4b07addd9b7541035c97b07a4236
StreakInTheSky <streakinthesky@gmail.com>
2017-05-27 00:21:28 -0700

    Move form states

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,25 @@
 import { combineReducers } from 'redux';
+import UserReducer from './user-reducer';
+import { combineForms } from 'react-redux-form';
+
+const initialLoginState = {
+  email: '',
+  password: ''
+}
+
+const initialRegistrationState = {
+  username: '',
+  email: '',
+  password: '',
+  passwordConfirm: ''
+};
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  user: UserReducer,
+  form: combineForms({
+    login: initialLoginState,
+    register: initialRegistrationState
+  }, 'form')
 });
 
 export default rootReducer;
