1acbb2a18de1c9d2cbf9ec91a9b4561368d3d1dc
StreakInTheSky <streakinthesky@gmail.com>
2017-06-10 03:21:40 -0700

    style thumbnails

--- src/components/curate-thumbnail.js
+++ src/components/curate-thumbnail.js
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function Thumbnail(props) {
   return (
-    <div><img src={props.src} /></div>
+    <div style={styles.imageContainer} ><img src={props.src} style={styles.image}/></div>
   )
+}
+
+const styles = {
+  imageContainer: {
+    display: 'inline-block',
+    position: 'relative',
+    margin: '0 5px',
+    width: '100px',
+    height: '100px',
+    overflow: 'hidden'
+  },
+  image: {
+    width: '100%',
+    position: 'absolute',
+    top: '50%',
+    transform: 'translateY(-50%)'
+  }
 }
